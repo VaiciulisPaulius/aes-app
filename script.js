7ed153e12e4325dbbc7a4b76b9f89b71367241d3
Paulius <pauliusvaiciulism@gmail.com>
2024-03-19 09:06:43 +0200

    fixed bugs

--- script.js
+++ script.js
@@ -24,7 +24,8 @@ function initOptions() {
     document.querySelector(".segment").oninput = function () {
         if (this.value < 1) {
             this.value = 1
-        }
+        } else if (this.value > 16)
+        this.value = 16
         document.querySelector(".segment").innerHTML = this.value;
     }
 }
@@ -51,14 +52,6 @@ function initVectorLengthWarning () {
     const warningElement = document.querySelector(".init-vector-warning");
     lengthWarning(inputValue, warningElement, 16);
 }
-// function inputTextLengthWarning () {
-//     const inputValue = document.querySelector(".init-vector").value;
-//     const warningElement = document.querySelector(".init-vector-warning");
-
-
-
-//     lengthWarning(inputValue, warningElement);
-// }
 function onSubmit() {
     console.log(document.querySelector(".unencrypted-text").value)
     let inputValue = document.querySelector(".unencrypted-text").value
@@ -96,12 +89,14 @@ function resultDecode() {
     onSubmitDecode(inputValue)
 }
 function inputDecode() {
-    let inputValue = document.querySelector(".unencrypted-text").innerHTML;
+    let inputValue = document.querySelector(".unencrypted-text").value;
     onSubmitDecode(inputValue)
 }
 function onSubmitDecode(input) {
     let inputValue = input
     const secretKey = document.querySelector(".secret-key").value
+    console.log("inputValue: " + inputValue)
+    console.log("Secret key:" + secretKey)
 
     if(secretKey.length != 16) return;
 
@@ -112,7 +107,7 @@ function onSubmitDecode(input) {
         let initVector = document.querySelector(".init-vector").value;
         if(initVector.length != 16) return;
 
-        document.querySelector(".result").innerHTML = cbcDecode(document.querySelector(".result").innerHTML, secretKey, initVector)
+        document.querySelector(".result").innerHTML = cbcDecode(inputValue, secretKey, initVector)
     }
     if(type == 3) {
         let initVector = document.querySelector(".init-vector").value;
@@ -121,111 +116,75 @@ function onSubmitDecode(input) {
         inputValue = paddingText(inputValue, segment)
         if(initVector.length != 16) return;
 
+        console.log(inputValue, secretKey, initVector, segment)
+
         document.querySelector(".result").innerHTML = cfbDecode(inputValue, secretKey, initVector, segment)
     }
 
     document.querySelector(".decode").style.display = "none"
 }
 function cbc(inputValue, secretKey, initVector) {
-    // An example 128-bit key
     let key = aesjs.utils.utf8.toBytes(secretKey);
-    console.log(key)
-    console.log(secretKey)
-    //let key = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 ];
-    
-    // The initialization vector (must be 16 bytes)
     let iv = aesjs.utils.utf8.toBytes(initVector)
-    console.log(iv)
-    console.log(initVector)
-    //let iv = [ 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34,35, 36 ];
-    
-    // Convert text to bytes (text must be a multiple of 16 bytes)
     let textBytes = aesjs.utils.utf8.toBytes(inputValue);
+
     let aesCbc = new aesjs.ModeOfOperation.cbc(key, iv);
     let encryptedBytes = aesCbc.encrypt(textBytes);
-    
-    // To print or store the binary data, you may convert it to hex
     let encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
+
     return encryptedHex
 }
 function cbcDecode(encryptedHex, secretKey, initVector) {
     let key = aesjs.utils.utf8.toBytes(secretKey);
     let iv = aesjs.utils.utf8.toBytes(initVector)
+    let encryptedBytes = aesjs.utils.hex.toBytes(encryptedHex);
 
-    var encryptedBytes = aesjs.utils.hex.toBytes(encryptedHex);
+    let aCbc = new aesjs.ModeOfOperation.cbc(key, iv);
+    let decryptedBytes = aCbc.decrypt(encryptedBytes);
+    let decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
 
-    var aCbc = new aesjs.ModeOfOperation.cbc(key, iv);
-    var decryptedBytes = aCbc.decrypt(encryptedBytes);
-    
-    var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
     return decryptedText
 }
 function ecb(inputValue, secretKey) {
-    // An example 128-bit key
     let key = aesjs.utils.utf8.toBytes(secretKey);
+    let textBytes = aesjs.utils.utf8.toBytes(inputValue);
     
-    // Convert text to bytes
-    var textBytes = aesjs.utils.utf8.toBytes(inputValue);
+    let aesEcb = new aesjs.ModeOfOperation.ecb(key);
+    let encryptedBytes = aesEcb.encrypt(textBytes);
     
-    var aesEcb = new aesjs.ModeOfOperation.ecb(key);
-    var encryptedBytes = aesEcb.encrypt(textBytes);
-    
-    // To print or store the binary data, you may convert it to hex
-    var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
+    let encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
     return encryptedHex
-    // "a7d93b35368519fac347498dec18b458"
-    
 }
 function ecbDecode(encryptedHex, secretKey) {
-    // When ready to decrypt the hex string, convert it back to bytes
-    var encryptedBytes = aesjs.utils.hex.toBytes(encryptedHex);
+    let encryptedBytes = aesjs.utils.hex.toBytes(encryptedHex);
     let key = aesjs.utils.utf8.toBytes(secretKey);
 
-    // Since electronic codebook does not store state, we can
-    // reuse the same instance.
-    //var aesEcb = new aesjs.ModeOfOperation.ecb(key);
-    var aesEcb = new aesjs.ModeOfOperation.ecb(key);
-    var decryptedBytes = aesEcb.decrypt(encryptedBytes);
-    
-    // Convert our bytes back into text
-    var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
+    let aesEcb = new aesjs.ModeOfOperation.ecb(key);
+    let decryptedBytes = aesEcb.decrypt(encryptedBytes);
+    let decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
+
     return decryptedText
 }
 function cfb(inputValue, secretKey, initVector, segment) {
-    console.log(inputValue)
-        // An example 128-bit key
-    var key = aesjs.utils.utf8.toBytes(secretKey);
-    
-    // The initialization vector (must be 16 bytes)
-    var iv = aesjs.utils.utf8.toBytes(initVector)
-    
-    // Convert text to bytes (must be a multiple of the segment size you choose below)
-    var textBytes = aesjs.utils.utf8.toBytes(inputValue);
-    
-    // The segment size is optional, and defaults to 1
-    var aesCfb = new aesjs.ModeOfOperation.cfb(key, iv, segment);
-    var encryptedBytes = aesCfb.encrypt(textBytes);
-    
-    // To print or store the binary data, you may convert it to hex
-    var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
+    let key = aesjs.utils.utf8.toBytes(secretKey);
+    let iv = aesjs.utils.utf8.toBytes(initVector)
+    let textBytes = aesjs.utils.utf8.toBytes(inputValue);
+
+    let aesCfb = new aesjs.ModeOfOperation.cfb(key, iv, segment);
+    let encryptedBytes = aesCfb.encrypt(textBytes);
+    let encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
+
     return encryptedHex
 }
 function cfbDecode(encryptedHex, secretKey, initVector, segment) {
-    var key = aesjs.utils.utf8.toBytes(secretKey);
-    
-    // The initialization vector (must be 16 bytes)
-    var iv = aesjs.utils.utf8.toBytes(initVector)
+    let key = aesjs.utils.utf8.toBytes(secretKey);
+    let iv = aesjs.utils.utf8.toBytes(initVector)
+    let encryptedBytes = aesjs.utils.hex.toBytes(encryptedHex);
 
-    // When ready to decrypt the hex string, convert it back to bytes
-    var encryptedBytes = aesjs.utils.hex.toBytes(encryptedHex);
+    let aesCfb = new aesjs.ModeOfOperation.cfb(key, iv, segment);
+    let decryptedBytes = aesCfb.decrypt(encryptedBytes);
+    let decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
 
-    // The cipher feedback mode of operation maintains internal state,
-    // so to decrypt a new instance must be instantiated.
-    var aesCfb = new aesjs.ModeOfOperation.cfb(key, iv, segment);
-    var decryptedBytes = aesCfb.decrypt(encryptedBytes);
-    
-    // Convert our bytes back into text
-    var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
     return decryptedText
 }
 function paddingText(input, multiple) {
@@ -254,7 +213,6 @@ function downloadFile() {
     URL.revokeObjectURL(link.href);
 }
 function readFile(input) {
-    console.log("hello")
     let file = input.files[0];
   
     let reader = new FileReader();
@@ -262,7 +220,6 @@ function readFile(input) {
     reader.readAsText(file);
   
     reader.onload = function() {
-      console.log(reader.result);
       document.querySelector(".unencrypted-text").value = reader.result
     };
   
